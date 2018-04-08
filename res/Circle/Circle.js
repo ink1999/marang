3fae2223b83483627fb357e8ae4a562eadcf9806
Park GangUn <gangun.park94@gmail.com>
2018-04-08 17:00:36 +0900

    012

--- res/Circle/Circle.js
+++ res/Circle/Circle.js
@@ -1,30 +1,29 @@
-var Diam1 = new Array(20);
-var Diam2 = new Array(20);
-var CirX = new Array(20);
-var CirY = new Array(20);
+var Diam1 = new Array(10);
+var Diam2 = new Array(10);
+var cx = new Array(10);
+var cy = new Array(10);
 
 function setup() {
   createCanvas(640, 480);
   frameRate(10);
 
-  for (var i=0; i<20; i++) {
+  for (var i=0; i<10; i++) {
     Diam1[i] = random(60);
     Diam2[i] = random(60);
-    CirX[i] = random(width);
-    CirY[i] = random(height);
+    cx[i] = random(640);
+    cy[i] = random(480);
   }
 }
 
 function draw() {
+  stroke(0);
+  strokeWeight(2);
   background(180);
   noFill();
-  strokeWeight(2);
 
-  for (var i=0; i<20; i++) {
-    ellipse(CirX[i], CirY[i], Diam1[i], Diam2[i]);
+  for (var i=0; i<10; i++) {
+    ellipse(cx[i], cy[i], Diam1[i], Diam2[i]);
     Diam1[i] += random(-10, 10);
     Diam2[i] += random(-10, 10);
-    Diam1[i] %= width;
-    Diam2[i] %= height;
   }
 }
