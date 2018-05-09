59506c71fa4800de41a616669e9bc0bc53d72d34
ink1999 <37644137+ink1999@users.noreply.github.com>
2018-05-10 03:49:31 +0900

    Add files via upload

---
+++ gal/gal1/ga1.js
@@ -0,0 +1,62 @@
+var bDragged;
+var angle = 0;
+var x = 200;
+var y = 200;
+var brush = 0;
+var brushSize = 0;
+var size = 0.05;
+var sizeSpeed = 0.10;
+var img;
+
+function preload() {
+  img = loadImage("2.jpg");
+}
+
+function setup() {
+  frameRate(300);
+  createCanvas(527, 412);
+  background(0);
+  rectMode(CENTER);
+}
+
+function draw() {
+  noFill();
+  rect(width/2, height/2, width, height);
+
+  fill(img.get(mouseX, mouseY));
+
+  if (bDragged) {
+    angle += 4;
+    translate(mouseX, mouseY);
+    rotate(radians(angle));
+    if ( brush == 0 ) {
+      noStroke();
+      rect(0, 0, 20+brushSize, 20+brushSize);
+    }
+    else if ( brush == 1 ) {
+      noStroke();
+      rect(0, 0, 40+brushSize, 5+brushSize);
+    }
+    else if ( brush ==2 ){
+      size += sizeSpeed;
+      if(size > 3.0 || size < 0.1 ) sizeSpeed = -sizeSpeed;
+      scale(size);
+      noStroke();
+      ellipse(0, 0, 30+brushSize, 30+brushSize);
+    }
+    bDragged = false;
+  }
+}
+
+function mouseDragged() {
+  bDragged = true;
+}
+function keyPressed(){
+  if(key === 'd') background(0);
+  if(key === '1') brush = 0;
+  if(key === '2') brush = 1;
+  if(key === '3') brush = 2;
+  if(keyCode === DOWN_ARROW) brushSize -= 3;
+  if(keyCode === UP_ARROW) brushSize += 3;
+  
+}
